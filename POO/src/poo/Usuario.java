76ca0c590aaa0e66efdafe60f87deb6e450ac3c0
GonzaloGNogales <41999907+GonzaloGNogales@users.noreply.github.com>
2018-12-29 15:50:58 +0100

    Update Usuario.java

--- POO/src/poo/Usuario.java
+++ POO/src/poo/Usuario.java
@@ -1,3 +1,76 @@
 package poo;
 public class Usuario {
+    private StringBuilder muro;
+    private int partidas_ganadas;
+    private int partidas_perdidas;
+    private int partidas_empatadas;
+    private Partida[] partidas_completas;
+    private Usuario[] solicitudes_amigos_pendientes;
+    
+    
+    
+    public void invitarAmigo (Usuario u) {
+        
+    }
+    
+    public void aceptarInvitacion (Usuario u) {
+        
+    }
+    
+    public void rechazarInvitacion (Usuario u) {
+        
+    }
+    
+    public void compartirPelicula (Pelicula p) {
+        
+    }
+    
+    public void compartirPelicula (Pelicula p, Usuario u) {
+        
+    }
+    
+    public void compartirCritica (Critica c) {
+        
+    }
+    
+    public void compartirCritica (Critica c, Usuario u) {
+        
+    }
+    
+    public void compartirPartida (Partida p) {
+        
+    }
+    
+    public void compartirPartida (Partida p, Usuario u) {
+        
+    }
+    
+    public void compartirTodo () {
+        
+    }
+    
+    public void compartirTodo (Usuario u) {
+        
+    }
+    
+    public void añadirPelicula (Pelicula p) {
+        
+    }
+    
+    public void añadirCritica (Critica c, Pelicula p) {
+        
+    }
+    
+    public void iniciarPartida (Usuario u) {
+        
+    }
+    
+    public void completarPartida (Partida p) {
+        
+    }
+    
+    public void setMuro (StringBuilder s) {
+        
+    }
+    
 }
