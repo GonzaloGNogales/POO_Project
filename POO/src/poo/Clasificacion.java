49e521055e1bf3a0f96405235a211eafb17462ee
GonzaloGNogales <41999907+GonzaloGNogales@users.noreply.github.com>
2018-12-29 15:45:36 +0100

    Update Clasificacion.java

--- POO/src/poo/Clasificacion.java
+++ POO/src/poo/Clasificacion.java
@@ -1,3 +1,28 @@
 package poo;
-public class Clasificacion{
+public class Clasificacion {
+    
+    public void actualizar (Partida p) {
+        //este es tipo partida
+    }
+    
+    public void visualizar () {
+        
+    }
+    
+    public void ordenarVictorias () {
+        
+    }
+    
+    public void ordenarPorcentaje () {
+        
+    }
+    
+    public void ordenarPuntos () {
+        
+    }
+    
+    public void volcarTXT (File f) {
+        
+    }
+    
 }
