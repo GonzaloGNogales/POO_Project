ca5b7611880926d445ff54b50fb5c0aee33d4d64
GonzaloGNogales <41999907+GonzaloGNogales@users.noreply.github.com>
2018-12-29 15:46:41 +0100

    Update Item.java

--- POO/src/poo/Item.java
+++ POO/src/poo/Item.java
@@ -1,3 +1,15 @@
 package poo;
 public class Item {
+    private String nombreUsuario;
+    private int jugadas;
+    private int ganadas;
+    private int perdidas;
+    private double porcentaje_victorias;
+    private int puntos;
+    
+    
+    public void detallePartidas (String nombre) {
+        
+    }
+    
 }
