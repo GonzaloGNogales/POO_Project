3a2d91a3c0c06e25d723db57c0f8e8fa3fdd5e66
GonzaloGNogales <41999907+GonzaloGNogales@users.noreply.github.com>
2018-12-29 15:47:41 +0100

    Update Pelicula.java

--- POO/src/poo/Pelicula.java
+++ POO/src/poo/Pelicula.java
@@ -1,3 +1,10 @@
 package poo;
 public class Pelicula {
+    private String titulo;
+    private int año;
+    private String director;
+    private String genero;
+    private String actor;
+    private String actriz;
+    
 }
