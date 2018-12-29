b312ab8e0e0a4049c97d591c0e5ece6ffcc850f8
GonzaloGNogales <41999907+GonzaloGNogales@users.noreply.github.com>
2018-12-29 15:48:12 +0100

    Update Peliculas.java

--- POO/src/poo/Peliculas.java
+++ POO/src/poo/Peliculas.java
@@ -1,3 +1,16 @@
 package poo;
 public class Peliculas {
+    
+    public Pelicula añadirPelicula (Pelicula p) {
+        
+        //devuelve una pelicula que hemos añadido
+        return null;
+    }
+    
+    public void verInfoPeliculas () {
+        
+        
+        
+    }
+    
 }
