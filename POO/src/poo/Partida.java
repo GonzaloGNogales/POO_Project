7ac901973a2c60fc3f29b062da2c831c87546567
GonzaloGNogales <41999907+GonzaloGNogales@users.noreply.github.com>
2018-12-29 15:47:12 +0100

    Update Partida.java

--- POO/src/poo/Partida.java
+++ POO/src/poo/Partida.java
@@ -1,3 +1,9 @@
 package poo;
 public class Partida {
-}
+    private int identificador;
+    private Usuario ganador;
+    private String resultado_final;
+    private int ptos_jugador1;
+    private int ptos_jugador2;
+    
+} 
