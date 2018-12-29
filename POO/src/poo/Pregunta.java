bda6cde21ac9fefd83634a08719d02f5e9711bdb
GonzaloGNogales <41999907+GonzaloGNogales@users.noreply.github.com>
2018-12-29 15:48:41 +0100

    Update Pregunta.java

--- POO/src/poo/Pregunta.java
+++ POO/src/poo/Pregunta.java
@@ -1,3 +1,16 @@
 package poo;
 public class Pregunta {
+    private int id;
+    private Pelicula pelicula;
+    private int puntos_jugador_1;
+    private int puntos_jugador_2;
+    
+    public void seleccionarPelicula (Peliculas film) {
+        
+    }
+    
+    public void ofrecerNuevaPista () {
+        
+    }
+    
 }
