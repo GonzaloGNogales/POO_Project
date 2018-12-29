6de29009cbe89ae3319bf5a662dc9a2c47671509
GonzaloGNogales <41999907+GonzaloGNogales@users.noreply.github.com>
2018-12-29 15:44:44 +0100

    Update Compartible.java

--- POO/src/poo/Compartible.java
+++ POO/src/poo/Compartible.java
@@ -1,3 +1,12 @@
 package poo;
 public class Compartible {
+    
+    public void compartir (Usuario u) {      
+        
+    }
+    
+    public void compartir (Usuario[] users) {
+        
+    }
+    
 }
